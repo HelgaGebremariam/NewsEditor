ffbc64ac242ee2cd55d80c3b36e6245c76d9a94d
helga <Helga.gebremariam@gmail.com>
2017-01-25 22:55:00 +0300

    Visual studio project

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/express-session/index.d.ts" />
+/// <reference path="globals/express-validator/index.d.ts" />
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/passport/index.d.ts" />
